126b9ded0f7be1c235b5d10d5dcfcfafc668b7d1
VedantVas <vedantvashish@gmail.com>
2025-07-18 22:31:11 +0530

    Multiple pointers

---
+++ linkedlist2.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{//self defined data structure 
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+
+    public:
+    Node(int data1){//concept of multiple pointers
+        data = data1;
+        next = nullptr;
+    }
+};
+
+int main(){
+
+    vector <int> arr={2,34,7,3,2};
+    Node* y = new Node(arr[0]);// calling pointer 2
+    //Node* y = new Node(arr[0],nullptr);//calling pointer 1
+    cout<<y->data<<endl;
+    return 0;
+}
