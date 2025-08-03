a82a459344eaba28117a054e22e1cd7c0f80942b
VedantVas <vedantvashish@gmail.com>
2025-08-03 18:12:19 +0530

    Delete Kth node of LL

---
+++ delete_kth_LL.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* removekthel(Node* head,int k){
+    if(head==NULL )return head;
+    if(k==1){
+        Node* temp = head;
+        head = temp->next;
+        free(temp);
+        return head;
+    }
+    int cnt = 0;
+    Node* temp = head;
+    Node* prev = NULL;
+    while(temp!=NULL){
+        cnt++;
+        if(cnt==k){
+            prev->next = temp->next;
+            free(temp);
+            break;
+        }
+        prev = temp;
+        temp= temp->next;
+        
+    }
+    return head;
+}
+
+void print(Node* head){
+    Node* temp = head;
+    while(temp!=nullptr){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+Node* convertARRtoLL(vector<int>arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+
+    }
+    return head;
+
+}
+
+int main(){
+    vector<int> nums = {1,2,3,4,5,6};
+    Node* head = convertARRtoLL(nums);
+    print(head);
+    int k;
+    cout<<"Enter the number of node you want to delete"<<endl;
+    cin>>k;
+    head = removekthel(head, k);
+    print(head);
+    return 0;
+}
